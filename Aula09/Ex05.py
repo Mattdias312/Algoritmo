eb6a12368d967ffc9a3c8c202795319cf7ef68d5
Mattdias312 <matheusf.dias12@gmail.com>
2023-10-09 22:05:26 -0300

    aula09 ex05

--- Aula09/Ex05.py
+++ Aula09/Ex05.py
@@ -1,19 +1,12 @@
 from random import randint
-lista = []
+lista = [0]*7
 
-for i in range(0, 6000):
-    lista.append(randint(1, 6))
+for _ in range(0, 6000):
+    n = randint(1, 6)
+    lista[n] = lista[n] + 1
 
-lado1 = lista.count(1)
-lado2 = lista.count(2)
-lado3 = lista.count(3)
-lado4 = lista.count(4)
-lado5 = lista.count(5)
-lado6 = lista.count(6)
+p = [0]*7
 
-print(f"Após 6000 jogadas sairam {lado1} o lado 1")
-print(f"Após 6000 jogadas sairam {lado2} o lado 2")
-print(f"Após 6000 jogadas sairam {lado3} o lado 3")
-print(f"Após 6000 jogadas sairam {lado4} o lado 4")
-print(f"Após 6000 jogadas sairam {lado5} o lado 5")
-print(f"Após 6000 jogadas sairam {lado6} o lado 6")
+for i in range(1,7):
+    p[i] = (lista[i] / 6000) * 100
+    print(f"{i} - {p[i]:.2f}% - {lista[i]}")
